dbfa8addd64c557b04793b866c277f9d28d0c2f6
Sameer Puri <sameer@users.noreply.github.com>
2019-01-17 14:40:03 -0600

    Add target for bare metal

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,7 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[cfg(target_os = "linux")]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    main();
-}
-
-#[cfg(target_os = "windows")]
-#[no_mangle]
-pub extern "C" fn mainCRTStartup() -> ! {
-    main();
-}
-
-#[no_mangle]
-pub extern "C" fn main() -> ! {
     loop {}
 }
